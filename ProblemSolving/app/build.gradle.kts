f876dadd5f01f03a207249e8e6e06d7b118620c9
SamiranDevo <samirankumar11@gmail.com>
2024-02-06 00:12:19 +0600

    package name change

--- ProblemSolving/app/build.gradle.kts
+++ ProblemSolving/app/build.gradle.kts
@@ -42,6 +42,7 @@ dependencies {
     implementation("com.google.android.material:material:1.11.0")
     testImplementation("junit:junit:4.14-SNAPSHOT")
     testImplementation("junit:junit:4.12")
+    testImplementation("org.testng:testng:6.9.6")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 }
--- ProblemSolving/app/src/test/java/com/samiran/problemsolving/ExampleUnitTest.kt
+++ ProblemSolving/app/src/test/java/com/samiran/problemsolving/ExampleUnitTest.kt
@@ -1,8 +1,8 @@
 package com.samiran.problemsolving
 
-import org.junit.Test
+import org.testng.Assert.assertEquals
+import org.testng.annotations.Test
 
-import org.junit.Assert.*
 
 /**
  * Example local unit test, which will execute on the development machine (host).
